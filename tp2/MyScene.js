9cc5dc4d757c3d7f77410a4ef5a877f0eb449e95
Guilherme Rego <up202207041@edu.fe.up.pt>
2025-02-24 16:12:47 +0000

    ex2 partial

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -4,6 +4,7 @@ import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram} from "./MyParallelogram.js";
 import { MyTriangleSmall } from "./MyTriangleSmall.js";
 import { MyTriangleBig } from "./MyTriangleBig.js";
+import { MyTangram } from "./MyTangram.js";
 
 /**
  * MyScene
@@ -34,6 +35,7 @@ export class MyScene extends CGFscene {
     this.parallelogram = new MyParallelogram(this);
     this.triangleSmall = new MyTriangleSmall(this);
     this.triangleBig = new MyTriangleBig(this);
+    this.tangram = new MyTangram(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -126,76 +128,9 @@ export class MyScene extends CGFscene {
       this.triangleBig.display();
     }
 
-    // Triangle 1?
-    this.pushMatrix();
-    this.translate(0, 0.5, 0);
-    this.rotate(Math.PI, 0, 0, 1); // escuro
-    this.triangleSmall.display();
-    this.popMatrix();
-
-    // Triangle 2
-    this.pushMatrix();
-    this.translate(-1, 0.5, 0);
-    this.triangle.display();
-    this.popMatrix();
-
-    // Triangle 3
-    this.pushMatrix();
-    this.translate(1.5, -0.5, 0);
-    this.scale(0.75, 0.75, 0.75);
-    this.triangleBig.display();
-    this.popMatrix();
-
-    // Triangle 4
-    this.pushMatrix();
-    this.translate(-2.75, 0, 0);
-    this.scale(0.75, 0.75, 0.75);
-    this.triangleSmall.display();
-    this.popMatrix();
-
-    // Triangle 5?
-    this.pushMatrix();
-    this.rotate(Math.PI, 1, -1, 0);
-    this.scale(0.5, 0.5, 0.5);
-    this.translate(-3, 3, 0);
-    this.triangle.display();
-    this.popMatrix();
-
-    // Parallelogram?
-    this.pushMatrix();
-    this.rotate(Math.PI, 0, 1, 0);
-    this.translate(-4.25, -0.25, 0);
-    this.scale(0.75, 0.75, 0.75);
-    this.parallelogram.display();
-    this.popMatrix();
-
-    // Diamond
-    var sca = [
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1,
-    ]
-
-    var trans = [
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      0, 0, 1, 0,
-      2, 1.62, 0, 1,
-    ]
-
-    var rot = [
-      Math.cos(-Math.PI / 6), -Math.sin(-Math.PI / 6), 0, 0,
-      Math.sin(-Math.PI / 6), Math.cos(-Math.PI / 6), 0, 0,
-      0, 0, 1, 0,
-      0, 0, 0, 1,
-    ]
-    this.pushMatrix();
-    this.multMatrix(sca);
-    this.multMatrix(trans);
-    this.multMatrix(rot);
-    this.diamond.display();
-    this.popMatrix();
+    if (this.tangram) {
+      this.tangram.display();
+    }
 
     // ---- END Primitive drawing section
   }
--- tp2/README.md
+++ tp2/README.md
@@ -2,14 +2,10 @@
 
 ## Group T11G09
 
-## TP 1 Notes
+## TP 2 Notes
 
-- In exercise 1, with the **MyDiamond.js** we learned how to create **MyTriangle.js**, and with that we were able to create the **MyParallelogram.js** with 4 triangles. To make the double-sided effect we repeat two times the same indexes, but in the second time we change first with third index and vice versa.
+- In exercise 2...
 
-- In exercise 2, as we already had the **MyTriangle.js**, it was easy to create **MyTriangleSmall** and **MyTriangleBig**.
+- In exercise 3...
 
-![Small Triangle](screenshots/G-t11g09-tp1-1small.PNG)
-<p align="center">Figure 2: Small Triangle</p>
-
-![Big Triangle](screenshots/G-t11g09-tp1-1big.PNG)
-<p align="center">Figure 1: Big Triangle</p>
+- In exercise 4...
